ab5cdef0bcca2b86d26c92d6be69a8092454231b
Paul J. Davis <paul.davis@tiledb.com>
2023-07-10 11:28:47 -0500

    Debugging

--- tiledb/as_built/test/unit_as_built.cc
+++ tiledb/as_built/test/unit_as_built.cc
@@ -35,6 +35,29 @@
 #include <tdb_catch.h>
 #include <iostream>
 
+#include <string_view>
+
+template <typename T>
+constexpr auto type_name() {
+  std::string_view name, prefix, suffix;
+#ifdef __clang__
+  name = __PRETTY_FUNCTION__;
+  prefix = "auto type_name() [T = ";
+  suffix = "]";
+#elif defined(__GNUC__)
+  name = __PRETTY_FUNCTION__;
+  prefix = "constexpr auto type_name() [with T = ";
+  suffix = "]";
+#elif defined(_MSC_VER)
+  name = __FUNCSIG__;
+  prefix = "auto __cdecl type_name<";
+  suffix = ">(void)";
+#endif
+  name.remove_prefix(prefix.size());
+  name.remove_suffix(suffix.size());
+  return name;
+}
+
 namespace as_built = tiledb::as_built;
 
 const std::string dump_str() noexcept {
@@ -89,6 +112,7 @@ TEST_CASE("as_built: Validate top-level key", "[as_built][top-level]") {
   auto x{dump_.value()["as_built"]};
   std::cerr << "XKCD1: " << dump_.value()["as_built"].dump(2) << std::endl;
   std::cerr << "XKCD2: " << x.dump(2) << std::endl;
+  std::cerr << type_name<decltype(x)>() << std::endl;
   CHECK(x.type() == nlohmann::detail::value_t::object);
   CHECK(!x.empty());
 }
